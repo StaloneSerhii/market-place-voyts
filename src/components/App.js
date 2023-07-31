54f4e733f2f61ba4005eb3602e479a4790e54092
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-31 14:31:29 +0300

    ,m

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-// import CatalogePage from 'pages/mainCataloge/catalogPage';
+import CatalogePage from 'pages/mainCataloge/catalogPage';
 // import Cards from './cards/cards';
 // import NavigateCategory from './categoryMainTop/navCategory';
 import Header from './header/header';
 // import Hero from './hero/hero';
 // import HeroCategory from './heroCategory/heroCategory';
-// import Advertisement from './advertisement/advertisement';
-// import TopSellPages from 'pages/topSellPages/topSellPages';
-// import LastPrev from 'pages/lastPrev/lastPrev';
-// import VideoContent from 'pages/videoContent/videoContent';
-// import Footer from './footer/footer';
+import Advertisement from './advertisement/advertisement';
+import TopSellPages from 'pages/topSellPages/topSellPages';
+import LastPrev from 'pages/lastPrev/lastPrev';
+import VideoContent from 'pages/videoContent/videoContent';
+import Footer from './footer/footer';
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
       <Hero />
       <HeroCategory />
       <Cards /> */}
-      {/* <CatalogePage />
+      <CatalogePage />
       <Advertisement />
       <TopSellPages />
       <LastPrev />
       <VideoContent />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
