d1663aa74926b130f04f8da175fa9c23ece31c69
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-08-11 13:29:09 +0300

    block buy

--- src/components/buyProduct/buyProduct.jsx
+++ src/components/buyProduct/buyProduct.jsx
@@ -1,49 +1,68 @@
-const BuyProduct = () => {
-    return (
-        <div>
-            <div>
-                <div><img src="/" alt="/" />
-                    <form>
-                        <label htmlFor="">
-                            <h3>Знайдемо потрібну запчастину</h3>
-                            <input type="text" placeholder="Номер або назва запчастини" />
-                        </label>
-                        <label htmlFor=""><input type="tel" placeholder="Телефон" /></label>
-                        <button>Надіслати запит</button>
-                    </form>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <h3>Назва запчастини... хрестовина з головками 4324-234-324 бла бла бла</h3>
-                    <div>
-                        <p>Код: <span>123132</span></p>
-                        <p>Артикул: <span>cr234</span></p>
-                        <p>Виробник: <span>Della Concerda</span></p>
-                        <span>Наявність</span>
-                        <span>3999.00 грн</span>
-                        <button>Купити</button>
-                    </div>
-                    <div>
-                        <p>Отримати консультацію</p>
-                        <span>+38(67)000-00-00</span>
-                        <button>Задати питання</button>
-                    </div>
-                </div>
-                <div>
-                    <h4>Аналони</h4>
-                    <div>
-                        <img src="/" alt="/" />
-                        <p>2313123</p>
-                        <p>Назва запчастини... хрестовина з головками 4324</p>
-                    </div>
-                </div>
-                <button>Опис</button>
-                <button>Застосування</button>
-                <button>ОБМ номер</button>
-            </div>
-        </div>
-    )
-}
+import { FcCallback } from 'react-icons/fc';
+import test from '../../image/testBuy.jpg';
+import { Link } from 'react-router-dom';
 
-export default BuyProduct
+const BuyProduct = () => {
+  return (
+    <div className="content__product">
+      <div>
+        <div>
+          <img src="/" alt="/" />
+          <form>
+            <label htmlFor="">
+              <h3>Знайдемо потрібну запчастину</h3>
+              <input type="text" placeholder="Номер або назва запчастини" />
+            </label>
+            <label htmlFor="">
+              <input type="tel" placeholder="Телефон" />
+            </label>
+            <button>Надіслати запит</button>
+          </form>
+        </div>
+      </div>
+      <div>
+        <h3 className="name__product">
+          Назва запчастини... хрестовина з головками 4324-234-324 бла бла бла
+        </h3>
+        <div className="block__info">
+          <div className="block__info--item">
+            <p>
+              Код: <span>123132</span>
+            </p>
+            <p>
+              Артикул: <span>cr234</span>
+            </p>
+            <p>
+              Виробник: <span>Della Concerda</span>
+            </p>
+            <p className="block__info--on">В наявності</p>
+            <span className="block__info--price">3999.00 грн</span>
+            <button type="button" className="formLogin__btn">
+              Купити
+            </button>
+          </div>
+          <div className="block__infoCenter">
+            <p> {<FcCallback />} Отримати консультацію</p>
+            <span>+38(67)000-00-00</span>
+            <button className="formLogin__btn">Задати питання</button>
+          </div>
+        </div>
+        <div className="block__analog">
+          <h4>Аналоги</h4>
+          <Link to="product/2" className="block__analog--info">
+            <img src={test} alt="/" width="70px" />
+            <p>Назва запчастини... хрестовина з головками 4324</p>
+            <p className="price">6805.90 грн</p>
+          </Link>
+        </div>
+        <div className="info__btn">
+          <button className="info__btn--details active__btn">Опис</button>
+          <button className="info__btn--details">Застосування</button>
+          <button className="info__btn--details">ОБМ номер</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BuyProduct;
